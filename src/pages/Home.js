d48c412c9f17744cf4fa85333abfb6f13a6765a6
Danwanaa94 <raufyussif04@gmail.com>
2022-03-29 14:28:16 +0000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div>
+    <h1>Welcome To My Home Page</h1>
+    <div>
+    <p>There is an unknown home stadium for the Black Stars. World Cup and Africa Cup 
+        of Nations qualifying matches have been played at the Essipong Stadium and Sekondi-Takoradi 
+        Stadium in Sekondi-Takoradi, the Len Clay Stadium, Kumasi Sports Stadium and Abrankese Stadium in Kumasi, 
+        the Cape Coast Sports Stadium in Cape Coast, the Accra Sports Stadium in the Accra and the Tamale Stadium 
+        in Tamale. Some smaller, regional stadia (stadiums) were also used in the 2002 Africa Cup of Nations qualifying
+         and 2004 African Cup of Nations qualification qualifying campaigns.</p>
+
+    <p>There is an unknown home stadium for the Black Stars. World Cup and Africa Cup of Nations qualifying
+         matches have been played at the Essipong Stadium and Sekondi-Takoradi Stadium in Sekondi-Takoradi, the
+          Len Clay Stadium, Kumasi Sports Stadium and Abrankese Stadium in Kumasi, the Cape Coast Sports Stadium in
+           Cape Coast, the Accra Sports Stadium in the Accra and the Tamale Stadium in Tamale. Some smaller, 
+           regional stadia (stadiums) were also used in the 2002 Africa Cup of Nations qualifying and 2004 African Cup of 
+           Nations qualification qualifying campaigns.</p>
+
+    <p>There is an unknown home stadium for the Black Stars. World Cup and Africa Cup of Nations qualifying matches 
+        have been played at the Essipong Stadium and Sekondi-Takoradi Stadium in Sekondi-Takoradi, the Len Clay Stadium,
+         Kumasi Sports Stadium and Abrankese Stadium in Kumasi, the Cape Coast Sports Stadium in Cape Coast, the Accra
+          Sports Stadium in the Accra and the Tamale Stadium in Tamale. Some smaller, regional stadia (stadiums) were 
+          also used in the 2002 Africa Cup of Nations qualifying and 2004 African Cup of Nations qualification
+           qualifying campaigns.</p>
+    </div>
+    </div>
+  )
+}
+
+export default withLayout(Home)
