19ca3e4a7205a07f792442709fb353ae5e17f891
Danwanaa94 <raufyussif04@gmail.com>
2022-04-01 16:24:39 +0000

    added some datA and files

---
+++ src/pages/Blog/details.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+function BlogDetails(){
+    const location = useLocation()
+    const [post, setPost] =useState({});
+    useEffect(()=>{
+        setPost(location.state.post);
+    },[location.state])
+
+    return(
+        <div>
+            {post && (
+                <div>
+                    <h2>{post.title}</h2>
+                    <p>{post.extract}</p>
+                    <img src= {`/image/${post.imgage}`} alt="" height={350} width={350}/>
+                    <p>{post.body}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+export default BlogDetails
